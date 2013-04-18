2c752ea1cf94d799851457260f01f67b4b1bff2b
denis <de1m0s242@gmail.com>
2013-04-18 18:53:15 +0800

    tracking skip action

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
 
-timeout 90
+timeout 30
 worker_processes 1 # Здесь тоже в зависимости от нагрузки, погодных условий и текущей фазы луны
 listen socket_file, :backlog => 1024
 pid pid_file
@@ -15,7 +15,7 @@ stdout_path log_file
 
 preload_app true # Мастер процесс загружает приложение, перед тем, как плодить рабочие процессы.
 
-GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=) # Решительно не уверен, что значит эта строка, но я решил ее оставить.
+#GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=) # Решительно не уверен, что значит эта строка, но я решил ее оставить.
 
 before_exec do |server|
   ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
