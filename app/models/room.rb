ced5ebd4046f3ebf9f29a10458ab7228d7f56cc5
de1mos <de1m0s242@gmail.com>
2013-07-31 00:56:30 +0800

    merge fuuuu

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,33 +1,7 @@
 class Room < ActiveRecord::Base
-<<<<<<< HEAD
   #attr_accessible :name, :users, :user_ids
-=======
-<<<<<<< HEAD
-  #attr_accessible :name, :users, :user_ids
-=======
-  attr_accessible :name, :users, :user_ids
->>>>>>> bbcbe771fecc123c9af07129281938150483f288
->>>>>>> origin/rooms
 
   has_many :users, through: :room_memberships
   has_many :room_memberships
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/rooms
-  scope 
-
-  scope :user_allowed, -> (user) begin
-  	if user
-
-  	else
-
-  end
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> bbcbe771fecc123c9af07129281938150483f288
->>>>>>> origin/rooms
 end
